5c35669b182d7e8e9ea0670e5485578e6625684a
Sathishreddy5 <sathishkumarreddy67@gmail.com>
2026-02-28 10:38:20 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.toastmessage;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnToast;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btnToast=findViewById(R.id.btnToast);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Toast toast= new Toast(getApplicationContext());
+               View view=getLayoutInflater().inflate(R.layout.custom_toast_layout,(ViewGroup) findViewById(R.id.viewContainer));
+        toast.setView(view);
+                TextView textMsg=view.findViewById(R.id.txtMsg);
+                textMsg.setText("Message Sent Successfully");
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+                toast.setGravity(Gravity.TOP|Gravity.END,0,0);
+            }
+        });
+
+
+
+
+
+    }
+}
